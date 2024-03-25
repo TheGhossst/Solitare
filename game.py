1a9f46985c127e5ebb8807f1c98af97ae88221bc
TheGhossst <nandukrishnapkm97@gmail.com>
2024-03-25 21:52:32 +0530

    idk

--- game.py
+++ game.py
@@ -26,7 +26,21 @@ class SolitaireGUI:
             self.canvas.create_rectangle(x, y, x + self.card_width, y + self.card_height, outline='black', fill='white')
     
     def setup_game(self):
-        pass
+        self.cards = []
+        for i in range(7):
+            for j in range(i + 1):
+                card = {'rank': 'blah', 'suit': 'Blah'}  
+                self.cards.append(card)
+                x = 20 + i * 100
+                y = 20 + j * 20
+                self.draw_card(card, x, y)
+                
+    def draw_card(self, card, x, y):
+        rank = card['rank']
+        suit = card['suit']
+        self.canvas.create_rectangle(x, y, x + self.card_width, y + self.card_height, outline='black', fill='white')
+        self.canvas.create_text(x + 10, y + 10, anchor='nw', text=rank, font=('Helvetica', '14'), fill='red')
+        self.canvas.create_text(x + 10, y + 30, anchor='nw', text=suit, font=('Helvetica', '14'), fill='red')
 
 def main():
     root = tk.Tk()
