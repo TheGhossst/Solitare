7d82baa157b70ce40ad812652a28cbf0d4f9a4a1
TheGhossst <nandukrishnapkm97@gmail.com>
2024-03-25 22:07:06 +0530

    idk

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 
 class SolitaireGUI:
     def __init__(self, master):
@@ -7,12 +8,24 @@ class SolitaireGUI:
         
         self.card_width = 71
         self.card_height = 96
+        
+        self.deck = self.generate_deck()
+        self.stock_pile = []
+        self.foundation_piles = [[] for _ in range(4)]
+        self.tableau_piles = [[] for _ in range(7)]
 
         self.canvas = tk.Canvas(self.master, width=800, height=600, bg='green')
         self.canvas.pack()
 
         self.draw_board()
         self.setup_game()
+        
+    def generate_deck(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+        deck = [{'rank': rank, 'suit': suit} for suit in suits for rank in ranks]
+        random.shuffle(deck)
+        return deck
         
     def draw_board(self):
         for i in range(7):
@@ -26,14 +39,24 @@ class SolitaireGUI:
             self.canvas.create_rectangle(x, y, x + self.card_width, y + self.card_height, outline='black', fill='white')
     
     def setup_game(self):
-        self.cards = []
         for i in range(7):
             for j in range(i + 1):
-                card = {'rank': 'blah', 'suit': 'Blah'}  
-                self.cards.append(card)
+                card = self.deck.pop(0)
+                self.tableau_piles[i].append(card)
                 x = 20 + i * 100
                 y = 20 + j * 20
                 self.draw_card(card, x, y)
+        for pile_index in range(7):
+            self.show_top_card('tableau', pile_index)
+        
+        for i in range(1, len(self.deck)):
+            self.stock_pile.append(self.deck.pop(0))
+        
+        self.show_top_card('stock')
+        self.show_top_card('foundation', 0)
+        
+    def show_top_card(self, pile_type, pile_index=None):
+        pass
                 
     def draw_card(self, card, x, y):
         rank = card['rank']
